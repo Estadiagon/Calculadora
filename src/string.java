5b0edc62911872c8308e085eb2a2239256b2472c
ESTADIAGON <juaeramirezr@correoudistrital.edu.co>
2018-08-15 17:41:36 -0500

    fallamos :(x2

--- src/string.java
+++ src/string.java
@@ -8,13 +8,6 @@
  *
  * @author Estudiantes
  */
-public class Calculadora {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+class string {
     
 }
