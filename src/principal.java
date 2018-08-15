3c7f6edc754cd2ee68e55761e076f0ba750cb4c2
ESTADIAGON <juaeramirezr@correoudistrital.edu.co>
2018-08-15 17:40:24 -0500

    fallamos :(

---
+++ src/principal.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class principal {
+   public static void main(string[] args){
+       Scanner miEscanner;
+       miEscanner=new Scanner(System.in);
+       int numero;
+       numero=miEscanner.nextInt();
+       
+       
+       CalculadoraTrigonometrica miCalculadora=new CalculadoraTrigonometrica();
+       
+               miCalculadora.convertirangulo(numero);
+               System.out.println(miCalculadora.seno());
+               System.out.println(miCalculadora.coseno());
+               System.out.println(miCalculadora.tangente());
+               
+   }
+    }
+
