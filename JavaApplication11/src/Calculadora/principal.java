c6f5b9b1b7e29619e54be290b4db69eb3e7c82eb
Estudiantes <Estudiantes@labing50415.udistrital.edu.co>
2018-08-24 11:51:03 -0500

    MegaCalculadora 1.10

---
+++ JavaApplication11/src/Calculadora/principal.java
@@ -0,0 +1,51 @@
+
+import java.awt.BorderLayout;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class principal {
+   public static void main(String[] args){
+       
+       
+       Calculadora miCalculadora=new Calculadora();
+       UltraCalculadora miUltra = new UltraCalculadora();
+       
+       
+       miCalculadora.setLayout(new BorderLayout());
+       miCalculadora.add(miUltra, BorderLayout.CENTER);
+       
+       miCalculadora.setVisible(true);
+       
+       
+       
+       
+       
+       /*
+       
+       
+       
+       Scanner miEscanner;
+       miEscanner=new Scanner(System.in);
+       
+       int numero=miEscanner.nextInt();
+       
+       
+       CalculadoraTrigonometrica miCalculadora=new CalculadoraTrigonometrica();
+       
+               miCalculadora.convertirangulo(numero);
+               System.out.println(miCalculadora.seno());
+               System.out.println(miCalculadora.coseno());
+               System.out.println(miCalculadora.tangente());
+         */      
+   }
+    }
+
